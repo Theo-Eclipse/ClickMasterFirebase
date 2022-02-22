73dad173eb4dc95b3960186829bdcab0ea6d4968
Theo Eclipse <84511904+Theo-Eclipse@users.noreply.github.com>
2022-02-22 13:12:38 +0200

    Intermediate Commit
    Firebase auth progress.

--- ClickMaster/Assets/Scripts/Firebase/AuthManager.cs
+++ ClickMaster/Assets/Scripts/Firebase/AuthManager.cs
@@ -7,16 +7,75 @@ using TMPro;
 
 public class AuthManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI ExceptionLabel;
-    // Start is called before the first frame update
-    void Start()
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth firebaseAuth;
+    public FirebaseUser firebaseUser;
+
+    private void Awake()
     {
-        
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                InitializeFirebase();
+            }
+            else 
+            {
+                ThrowLoginException($"Couldn't resolve all the depencies! status: {dependencyStatus.ToString()}");
+                ThrowRegistrationException($"Couldn't resolve all the depencies! status: {dependencyStatus.ToString()}");
+            }
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeFirebase() 
     {
-        
+        Debug.Log("Initializing firebase auth...");
+        firebaseAuth = FirebaseAuth.DefaultInstance;
+        UIController.instance.LoginButton.onClick.AddListener(TryLogin);
+        UIController.instance.RegisterButton.onClick.AddListener(TryRegister);
+    }
+
+    private void TryLogin() 
+    {
+    }
+
+    private void TryRegister() 
+    {
+    }
+
+    private IEnumerator Login(string _email, string _password) 
+    {
+        var LoginTask = firebaseAuth.SignInWithEmailAndPasswordAsync(_email, _password);
+
+        //Show loading screen
+        UIController.instance.ShowLoading(true);
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+        UIController.instance.ShowLoading(false);
+        if (LoginTask.Exception != null)
+        {
+            Debug.LogErrorFormat($"Failed to login, with exception: {LoginTask.Exception}");
+            FirebaseException ex = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)ex.ErrorCode;
+            ThrowLoginException($"Failed to login, with exception: {errorCode.ToString()}");
+        }
+        else 
+        {
+            firebaseUser = LoginTask.Result;
+            Debug.Log($"User logged in seccessfuly! as username: {firebaseUser.DisplayName}, with email: {firebaseUser.Email}");
+            UIController.instance.ShowClickerScreen();
+        }
+    }
+
+    private void ThrowLoginException(string exception_message) 
+    {
+        UIController.instance.LoginExceptionField.gameObject.SetActive(true);
+        UIController.instance.LoginExceptionField.text = exception_message;
+    }
+    private void ThrowRegistrationException(string exception_message)
+    {
+        UIController.instance.RegistrationExceptionField.gameObject.SetActive(true);
+        UIController.instance.RegistrationExceptionField.text = exception_message;
     }
 }
--- ClickMaster/Assets/UIController.cs
+++ ClickMaster/Assets/UIController.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
+[RequireComponent(typeof(RectTransform), typeof(Canvas))]
 public class UIController : MonoBehaviour
 {
+    public static UIController instance { get; private set; }
+    [Header("Screens")]
     [SerializeField] private GameObject WelcomeScreen;
     [SerializeField] private GameObject RegistrationScreen;
     [SerializeField] private GameObject LoginScreen;
     [SerializeField] private GameObject ClickerScreen;
     [SerializeField] private GameObject LoadingScreen;
+
+    [Header("Login Form")]
+    public TMP_InputField UserNameField;
+    public TMP_InputField UserPasswordField;
+    public Button LoginButton;
+    public TextMeshProUGUI LoginExceptionField;
+
+    [Header("Register Form")]
+    public TMP_InputField NewUserFullNameField;
+    public TMP_InputField NewUserEmailField;
+    public TMP_InputField NewUserNickNameField;
+    public TMP_InputField NewUserPasswordField;
+    public Button RegisterButton;
+    public TextMeshProUGUI RegistrationExceptionField;
+
+    public 
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         ShowWelcomeScreen();
     }
 
@@ -34,6 +56,8 @@ public class UIController : MonoBehaviour
 
     public void ShowRegistrationScreen()
     {
+        ClearRegistrationForm();
+        NewUserFullNameField.Select();// Select the first field.
         WelcomeScreen.SetActive(false);
         RegistrationScreen.SetActive(true);
         LoginScreen.SetActive(false);
@@ -43,6 +67,8 @@ public class UIController : MonoBehaviour
 
     public void ShowLoginScreen()
     {
+        ClearLoginForm();
+        UserNameField.Select();// Select the first field.
         WelcomeScreen.SetActive(false);
         RegistrationScreen.SetActive(false);
         LoginScreen.SetActive(true);
@@ -60,5 +86,21 @@ public class UIController : MonoBehaviour
     }
 
     public void ShowLoading(bool loading) => LoadingScreen.SetActive(loading);
-    
+
+    private void ClearRegistrationForm() 
+    {
+        NewUserFullNameField.text = "";
+        NewUserEmailField.text = "";
+        NewUserNickNameField.text = "";
+        NewUserPasswordField.text = "";
+        RegistrationExceptionField.gameObject.SetActive(false);
+    }
+
+    private void ClearLoginForm()
+    {
+        UserNameField.text = "";
+        UserPasswordField.text = "";
+        LoginExceptionField.gameObject.SetActive(false);
+    }
+
 }
