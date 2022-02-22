5a71c69fdfe1b56665ccb4806ec36207c14ae32e
Theo Eclipse <84511904+Theo-Eclipse@users.noreply.github.com>
2022-02-22 12:08:49 +0200

    Added Exception Label
    Red text for showing up exceptions in process of login or registration.

---
+++ ClickMaster/Assets/Scripts/Firebase/AuthManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+public class AuthManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI ExceptionLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
